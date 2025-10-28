8898a16be8e88e827c136a1a727c0f664dd39786
PecoraroAmal <amal.pecoraro@gmail.com>
2025-10-28 18:16:38 +0100

    Version 3.4 policy update

--- sw.js
+++ sw.js
@@ -1,21 +1,21 @@
 const CACHE_NAME = 'beskytter-cache-v1';
 const urlsToCache = [
-  '/Beskytter/index.html?v=3.3',
-  '/Beskytter/edit.html?v=3.3',
-  '/Beskytter/privacy-policy.html?v=3.3',
-  '/Beskytter/cookie-policy.html?v=3.3',
-  '/Beskytter/info.html?v=3.3',
-  '/Beskytter/informazioni.html?v=3.3',
-  '/Beskytter/download.html?v=3.3',
-  '/Beskytter/scarica.html?v=3.3',
-  '/Beskytter/css/style.css?v=3.3',
-  '/Beskytter/css/policy.css?v=3.3',
-  '/Beskytter/js/crypto.js?v=3.3',
-  '/Beskytter/js/edit.js?v=3.3',
-  '/Beskytter/js/home.js?v=3.3',
-  '/Beskytter/js/info.js?v=3.3',
-  '/Beskytter/js/app.js?v=3.3',
-  '/Beskytter/js/utils.js?v=3.3',
+  '/Beskytter/index.html?v=3.4',
+  '/Beskytter/edit.html?v=3.4',
+  '/Beskytter/privacy-policy.html?v=3.4',
+  '/Beskytter/cookie-policy.html?v=3.4',
+  '/Beskytter/info.html?v=3.4',
+  '/Beskytter/informazioni.html?v=3.4',
+  '/Beskytter/download.html?v=3.4',
+  '/Beskytter/scarica.html?v=3.4',
+  '/Beskytter/css/style.css?v=3.4',
+  '/Beskytter/css/policy.css?v=3.4',
+  '/Beskytter/js/crypto.js?v=3.4',
+  '/Beskytter/js/edit.js?v=3.4',
+  '/Beskytter/js/home.js?v=3.4',
+  '/Beskytter/js/info.js?v=3.4',
+  '/Beskytter/js/app.js?v=3.4',
+  '/Beskytter/js/utils.js?v=3.4',
   '/Beskytter/assets/apple-touch-icon.png',
   '/Beskytter/assets/beskytter-logo.png',
   '/Beskytter/assets/favicon-96x96.png',
@@ -81,11 +81,11 @@ self.addEventListener('fetch', event => {
           return fetchAndUpdateCache(event.request);
         }
         // If offline and not in cache, return fallback
-        return caches.match('/Beskytter/index.html?v=3.3');
+        return caches.match('/Beskytter/index.html?v=3.4');
       })
       .catch(error => {
         console.error('Fetch failed:', error);
-        return caches.match('/Beskytter/index.html?v=3.3');
+        return caches.match('/Beskytter/index.html?v=3.4');
       })
   );
 });
