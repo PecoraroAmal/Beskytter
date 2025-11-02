ef4be881e23699716bc9a0ed1b90828c6580ed4b
PecoraroAmal <amal.pecoraro@gmail.com>
2025-11-02 23:21:29 +0100

    Version 3.17

--- sw.js
+++ sw.js
@@ -1,21 +1,21 @@
-const CACHE_NAME = 'beskytter-cache-v3.16';
+const CACHE_NAME = 'beskytter-cache-v3.17';
 const urlsToCache = [
-  '/Beskytter/index.html?v=3.16',
-  '/Beskytter/edit.html?v=3.16',
-  '/Beskytter/privacy-policy.html?v=3.16',
-  '/Beskytter/cookie-policy.html?v=3.16',
-  '/Beskytter/info.html?v=3.16',
-  '/Beskytter/informazioni.html?v=3.16',
-  '/Beskytter/download.html?v=3.16',
-  '/Beskytter/scarica.html?v=3.16',
-  '/Beskytter/css/style.css?v=3.16',
-  '/Beskytter/css/policy.css?v=3.16',
-  '/Beskytter/js/crypto.js?v=3.16',
-  '/Beskytter/js/edit.js?v=3.16',
-  '/Beskytter/js/home.js?v=3.16',
-  '/Beskytter/js/info.js?v=3.16',
-  '/Beskytter/js/app.js?v=3.16',
-  '/Beskytter/js/utils.js?v=3.16',
+  '/Beskytter/index.html?v=3.17',
+  '/Beskytter/edit.html?v=3.17',
+  '/Beskytter/privacy-policy.html?v=3.17',
+  '/Beskytter/cookie-policy.html?v=3.17',
+  '/Beskytter/info.html?v=3.17',
+  '/Beskytter/informazioni.html?v=3.17',
+  '/Beskytter/download.html?v=3.17',
+  '/Beskytter/scarica.html?v=3.17',
+  '/Beskytter/css/style.css?v=3.17',
+  '/Beskytter/css/policy.css?v=3.17',
+  '/Beskytter/js/crypto.js?v=3.17',
+  '/Beskytter/js/edit.js?v=3.17',
+  '/Beskytter/js/home.js?v=3.17',
+  '/Beskytter/js/info.js?v=3.17',
+  '/Beskytter/js/app.js?v=3.17',
+  '/Beskytter/js/utils.js?v=3.17',
   '/Beskytter/assets/apple-touch-icon.png',
   '/Beskytter/assets/beskytter-logo.png',
   '/Beskytter/assets/favicon-96x96.png',
@@ -81,11 +81,11 @@ self.addEventListener('fetch', event => {
           return fetchAndUpdateCache(event.request);
         }
         // If offline and not in cache, return fallback
-        return caches.match('/Beskytter/index.html?v=3.16');
+        return caches.match('/Beskytter/index.html?v=3.17');
       })
       .catch(error => {
         console.error('Fetch failed:', error);
-        return caches.match('/Beskytter/index.html?v=3.16');
+        return caches.match('/Beskytter/index.html?v=3.17');
       })
   );
 });
